403bf75ea14e7e124fce1ae4fed6920a801cec1b
kkk <kutimskyi@gmail.com>
2023-10-01 17:06:42 +0300

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,27 @@
 import { Tooltip } from "./popover";
 
-const form = document.querySelector('.form');
+const form = document.querySelector(".form");
 
 const popovers = {
-    submit: {
-        header : "Popover title",
-        message: 'И вот потрясающий контент. Это очень увлекательно. Верно?',
-    },
-}
+  submit: {
+    header: "Popover title",
+    message: "И вот потрясающий контент. Это очень увлекательно. Верно?",
+  },
+};
 
 const tooltipFactory = new Tooltip();
-form.querySelector('button').addEventListener('focus',(e) =>{
-    e.target.addEventListener('click', (e)=>{  
-        e.preventDefault();
-        console.log(form.querySelector('.popover'))
-        if(document.querySelector('.popover') === null){
-            tooltipFactory.showTooltip(popovers[e.target.type].header,popovers[e.target.type].message, e.target);
-        } else {
-            document.querySelector('.popover').classList.toggle('deactive')
-        }
-
-    })
-})
-
+form.querySelector("button").addEventListener("focus", (e) => {
+  e.target.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(form.querySelector(".popover"));
+    if (document.querySelector(".popover") === null) {
+      tooltipFactory.showTooltip(
+        popovers[e.target.type].header,
+        popovers[e.target.type].message,
+        e.target
+      );
+    } else {
+      document.querySelector(".popover").classList.toggle("deactive");
+    }
+  });
+});
--- src/js/popover.js
+++ src/js/popover.js
@@ -1,23 +1,22 @@
 export class Tooltip {
-    constructor() {
-    }
-    showTooltip(header,message, element) {
-        const tooltipElement = document.createElement('DIV');
-        const tooltipHeader = document.createElement('h2');
-        const tooltipMessage = document.createElement('div');
-        tooltipHeader.textContent = header;
-        tooltipMessage.textContent = message;
-        tooltipElement.classList.add('popover');
-        tooltipHeader.classList.add('popover-header');
-        tooltipMessage.classList.add('popover-message');
-        document.body.appendChild(tooltipElement);
-        tooltipElement.prepend(tooltipHeader)
-        tooltipElement.append(tooltipMessage)
-        const { right, top } = element.getBoundingClientRect();
-        tooltipElement.style.left = right - element.offsetWidth/2 - tooltipElement.offsetWidth/2+ 'px' ;
-        tooltipElement.style.top = top - tooltipElement.offsetHeight - 5 + 'px';
+  constructor() {}
+  showTooltip(header, message, element) {
+    const tooltipElement = document.createElement("DIV");
+    const tooltipHeader = document.createElement("h2");
+    const tooltipMessage = document.createElement("div");
+    tooltipHeader.textContent = header;
+    tooltipMessage.textContent = message;
+    tooltipElement.classList.add("popover");
+    tooltipHeader.classList.add("popover-header");
+    tooltipMessage.classList.add("popover-message");
+    document.body.appendChild(tooltipElement);
+    tooltipElement.prepend(tooltipHeader);
+    tooltipElement.append(tooltipMessage);
+    const { right, top } = element.getBoundingClientRect();
+    tooltipElement.style.left =
+      right - element.offsetWidth / 2 - tooltipElement.offsetWidth / 2 + "px";
+    tooltipElement.style.top = top - tooltipElement.offsetHeight - 5 + "px";
 
-        return tooltipElement;
-    }
-
+    return tooltipElement;
+  }
 }
