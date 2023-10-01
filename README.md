85eab8f13aa5f0470431e0d09adb964e578cea64
kkk <kutimskyi@gmail.com>
2023-10-01 17:01:12 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Домашнее задание к занятию "5.HTML формы"
+[![Build status](https://ci.appveyor.com/api/projects/status/vc0ir6gkj73pn9ow?svg=true)](https://ci.appveyor.com/project/Kutimskii/ajs-popover) \
---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+import { Tooltip } from "./popover";
+
+const form = document.querySelector('.form');
+
+const popovers = {
+    submit: {
+        header : "Popover title",
+        message: 'И вот потрясающий контент. Это очень увлекательно. Верно?',
+    },
+}
+
+const tooltipFactory = new Tooltip();
+form.querySelector('button').addEventListener('focus',(e) =>{
+    e.target.addEventListener('click', (e)=>{  
+        e.preventDefault();
+        console.log(form.querySelector('.popover'))
+        if(document.querySelector('.popover') === null){
+            tooltipFactory.showTooltip(popovers[e.target.type].header,popovers[e.target.type].message, e.target);
+        } else {
+            document.querySelector('.popover').classList.toggle('deactive')
+        }
+
+    })
+})
+
